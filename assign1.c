d3970778b71acce6e2adc97c7d68c6e8bcf8b981
Nathan Shepherd <shephern@os-class.engr.oregonstate.edu>
2017-04-21 15:05:24 -0700

    Compiles currently, incomplete

--- assign1.c
+++ assign1.c
@@ -16,15 +16,60 @@ struct item{
 
 struct args{
         long tid;
-        struct item (*buffer);
+        struct item *buffer;
         int *buf_size;
 };
 
-void *consume(void *tid, void *buffer)
+void *consume(void *passed_arg);
+void *produce(void *passed_arg);
+unsigned int get_random(int limit);
+
+int main(int argc, char **argv)
 {
-        struct args *a = (struct args*)tid;
-        sleep(a->sleep_time);
-        return (void*)printf("Hello from thread %ld!", a->tid);
+        if (argc != 3) {
+                printf("USAGE: assign1 num_producers num_consumers");
+                exit(EXIT_FAILURE);
+        }
+
+        unsigned long init[4] = {0x142, 0x253, 0x364, 0x475};
+        unsigned long length = 4;
+        init_by_array(init, length);
+
+        int num_prod = atoi(argv[1]);
+        int num_cons = atoi(argv[2]);
+        int num_thrd = num_prod + num_cons;
+        struct item buffer[32];
+        unsigned int buf_size = 0;
+
+        pthread_t threads[num_thrd];
+        struct args a[num_thrd];
+
+        if (pthread_mutex_init(&buffer_mutex, NULL) != 0) {
+                printf("ERROR: Mutex init failed");
+                exit(EXIT_FAILURE);
+        }
+        
+        a->buffer = &buffer[0];
+        a->buf_size = &buf_size;
+
+        for (long i = 0; i < num_prod; ++i) {
+                a->tid = i;
+
+                pthread_create(&(threads[i]),
+                               NULL,
+                               produce,
+                               (void*)&a[i]);
+        }
+        
+        return 0;
+}
+
+void *consume(void *passed_arg)
+{
+        struct args *a = (struct args*)passed_arg;
+        
+        printf("Hello from thread %d!", *(a->buf_size));
+        return NULL;
         /*
          * TODO: consumer function
          * * Consumes struct by removing it from the buffer
@@ -37,12 +82,23 @@ void *consume(void *tid, void *buffer)
 void *produce(void *passed_arg)
 {
         struct args *a = (struct args*)passed_arg;
+        while(1 == 1) {
+                while(*(a->buf_size) >= 32)
+                        sleep(0.01);
 
-        while((int *)a->buf_size >= 32)
-                sleep(0.01);
+                pthread_mutex_lock(&buffer_mutex);
         
+                struct item *tmp = malloc(sizeof(struct item));
+                tmp->value = get_random(20);
+                tmp->sleep_time = get_random(8) + 2;
 
-        pthread_mutex_lock(&buffer_mutex);
+                a->buffer[*(a->buf_size) - 1] = *tmp;
+                (int *)a->buf_size++;
+
+                pthread_mutex_unlock(&buffer_mutex);
+                
+                sleep(get_random(5) + 3);
+        }
 
         /*
          * TODO: producer function
@@ -71,7 +127,7 @@ unsigned int get_random(int limit)
         if (ecx & 0x40000000) {
                 //Use rdrand
                 __asm__ __volatile__(
-                                     "rdrand %eax;"
+                                     "rdrand %0;"
                                      : "=a"(eax)
                                      : "a"(eax)
                                     );
@@ -82,44 +138,4 @@ unsigned int get_random(int limit)
         }
         return random;
 }
-
-int main(int argc, char **argv)
-{
-        if (argc != 3) {
-                printf("USAGE: assign1 num_producers num_consumers");
-                exit(EXIT_FAILURE);
-        }
-
-        unsigned long init[4] = {0x142, 0x253, 0x364, 0x475}
-        unsigned long length = 4;
-        init_by_array(init, length);
-
-        int num_prod = atoi(argv[1]);
-        int num_cons = atoi(argv[2]);
-        int num_thrd = num_producer + num_consumer;
-        struct item buffer[32];
-        unsigned int buf_size = 0;
-
-        pthread_t threads[num_thrd];
-        struct args a[num_thrd];
-
-        if (pthread_mutex_init(&buffer_lock, NULL) != 0) {
-                printf("ERROR: Mutex init failed");
-                exit(EXIT_FAILURE);
-        }
-        
-        a->buffer = &buffer;
-        a->buf_size = &buf_size;
-
-        for (long i = 0; i < num_prod; ++i) {
-                a->tid = i;
-
-                pthread_create(&(threads[i]),
-                               NULL,
-                               produce,
-                               (void*)&a[i]);
-        }
-}
-
-
 
