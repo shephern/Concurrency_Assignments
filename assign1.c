1053688a0c6c1e661255e803b70df1b157769c07
Nathan Shepherd <shephern@os-class.engr.oregonstate.edu>
2017-04-21 17:30:52 -0700

    Threads just starting to work

--- assign1.c
+++ assign1.c
@@ -8,6 +8,7 @@
 
 
 pthread_mutex_t buffer_mutex;
+pthread_mutex_t print_mutex;
 
 struct item{
         long value;
@@ -48,19 +49,37 @@ int main(int argc, char **argv)
                 printf("ERROR: Mutex init failed");
                 exit(EXIT_FAILURE);
         }
+        if (pthread_mutex_init(&print_mutex, NULL) != 0) {
+                printf("ERROR: Print Mutex Init failed");
+                exit(EXIT_FAILURE);
+        }
         
-        a->buffer = &buffer[0];
-        a->buf_size = &buf_size;
-
         for (long i = 0; i < num_prod; ++i) {
-                a->tid = i;
+                a[i].tid = i;
+                printf("%d\n", a[i].tid);
+                a[i].buf_size = &buf_size;
+                a[i].buffer = &buffer[0];
 
                 pthread_create(&(threads[i]),
                                NULL,
                                produce,
                                (void*)&a[i]);
         }
-        
+
+        for (long i = num_prod-1; i < num_thrd; ++i) {
+                a[i].tid = i;
+                a[i].buf_size = &buf_size;
+                a[i].buffer = &buffer[0];
+
+                pthread_create(&(threads[i]),
+                               NULL,
+                               consume,
+                               (void*)&a[i]);
+        }
+
+        for (long i = 0; i < num_thrd; ++i) {
+                pthread_join(threads[i], NULL);
+        }
         return 0;
 }
 
@@ -68,7 +87,7 @@ void *consume(void *passed_arg)
 {
         struct args *a = (struct args*)passed_arg;
         
-        printf("Hello from thread %d!", *(a->buf_size));
+        printf("Consumer %d!", *(a->buf_size));
         return NULL;
         /*
          * TODO: consumer function
@@ -81,6 +100,8 @@ void *consume(void *passed_arg)
 
 void *produce(void *passed_arg)
 {
+        printf("hello");
+        return NULL;
         struct args *a = (struct args*)passed_arg;
         while(1 == 1) {
                 while(*(a->buf_size) >= 32)
@@ -93,8 +114,12 @@ void *produce(void *passed_arg)
                 tmp->sleep_time = get_random(8) + 2;
 
                 a->buffer[*(a->buf_size) - 1] = *tmp;
-                (int *)a->buf_size++;
+                *(a->buf_size)++;
 
+                pthread_mutex_lock(&print_mutex);
+                printf("Producer %d created item of %d value", 
+                        1, 12);
+                pthread_mutex_unlock(&print_mutex);
                 pthread_mutex_unlock(&buffer_mutex);
                 
                 sleep(get_random(5) + 3);
